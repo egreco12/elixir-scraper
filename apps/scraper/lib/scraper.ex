ee51c9265a7b23f29c8e29b787f5a2b30ae2d786
Evan Greco <evan.greco@legitscript.com>
2019-06-23 13:02:05 -0700

    Add basic supervision tree for scraper app

--- apps/scraper/lib/scraper.ex
+++ apps/scraper/lib/scraper.ex
@@ -1,18 +1,9 @@
 defmodule Scraper do
-  @moduledoc """
-  Documentation for Scraper.
-  """
+  use Application
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Scraper.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def start(_type, _args) do
+    # Although we don't use the supervisor name below directly,
+    # it can be useful when debugging or introspecting the system.
+    Scraper.Supervisor.start_link(name: Scraper.Supervisor)
   end
 end
